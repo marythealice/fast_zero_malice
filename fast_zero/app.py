81482636364b059f2a2a61aed4c19237d26ad323
Maria Alice <nantesmalice@gmail.com>
2024-11-02 18:57:56 -0400

    add post method

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 
+from http import HTTPStatus
+
+from schemas import Message, UserSchema, UserPublic
+
+
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello World'}
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema): 
+    '''note que o cliente envia no modelo userschema,
+     mas a resposta é userpublic, q n mostra sua senha'''
+    return user
