06b9264ab5eab66690160bbb949f3307f0d6527a
Maria Alice <nantesmalice@gmail.com>
2024-11-06 10:30:19 -0400

    implementando rotas crud

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,19 +1,50 @@
-from fastapi import FastAPI
-
 from http import HTTPStatus
 
-from schemas import Message, UserSchema, UserPublic
+from fastapi import FastAPI, HTTPException
 
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
+
+database = []  # database vazia p testagem
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello World'}
 
+
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema): 
-    '''note que o cliente envia no modelo userschema,
-     mas a resposta é userpublic, q n mostra sua senha'''
-    return user
+def create_user(user: UserSchema):
+    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+    database.append(user_with_id)
+    return user_with_id
+
+
+@app.get('/users/', response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    user_with_id = UserDB(id=user_id, **user.model_dump())
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)  # cruD
+def delete_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    del database[user_id - 1]
+
+    return {'message': 'User deleted!'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,16 +1,10 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
-
-from fast_zero.app import app
-
 """nomear cada função de teste relacionado ao que ela está testando
 no caso abaixo, ler a url do site """
 
 
-def test_read_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)  # Arrange (organizando o teste)
-
+def test_read_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')  # act (ação, execução de um bloco de test)
     print(response)
 
@@ -23,3 +17,55 @@ def test_read_root_deve_retornar_ok_e_ola_mundo():
     assert (but now making sure that the
     expected response was the same as the received one
     """
+
+
+def test_create_user(client):
+    response = client.post(  # testando UserSchema
+        '/users/',
+        json={
+            'username': 'testusername',
+            'email': 'test@test.com',
+            'password': 'password',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    # voltou o status correto? se sim..
+    # agora vamos validar o UserPublic
+    assert response.json() == {
+        'id': 1,
+        'username': 'testusername',
+        'email': 'test@test.com',
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {'id': 1, 'username': 'testusername', 'email': 'test@test.com'}
+        ]
+    }
+
+
+def test_update_users(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'password': '123',
+            'id': 1,
+            'username': 'testusername2',
+            'email': 'test@test.com',
+        },
+    )
+    assert response.json() == {
+        'id': 1,
+        'username': 'testusername2',
+        'email': 'test@test.com',
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.json() == {'message': 'User deleted!'}
